fe00e4d01d26a1c3ee5aac708da950c18c859666
Jiya Mehta <jiyamehta2110@gmail.com>
2025-02-23 20:29:02 +0530

    Wrote JS logic and debugged it

---
+++ app.js
@@ -0,0 +1,64 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+const genCompChoice = () => {
+    const options = ["Rock", "Paper", "Scissors"];
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
+};
+
+const drawGame = () => {
+    msg.innerText = "Game was Draw. Play again!"
+    msg.style.backgroundColor = "#081b31";
+};
+
+const showWinner = (userWin, userChoice, compChoice) => {
+    if(userWin) {
+        userScore++;
+        userScorePara.innerText = userScore;
+        msg.innerText = `You win! ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
+    } else {
+        compScore++;
+        compScorePara.innerText = compScore;
+        msg.innerText = `You lose! ${compChoice} beats ${userChoice}`;
+        msg.style.backgroundColor = "red";
+    }
+};
+
+const playGame = (userChoice) => {
+    // generate computer choice 
+    const compChoice = genCompChoice();
+
+    if(userChoice === compChoice)
+    {
+        // game draw 
+        drawGame();
+    } else {
+        let userWin = true;
+        if(userChoice === "Rock") {
+            // scissors, paper
+            userWin = compChoice === "Paper" ? false : true;
+        } else if(userChoice === "Paper") {
+            // rock, scissors
+            userWin = compChoice === "Scissors" ? false : true;
+        } else {
+            // rock, paper
+            userWin = compChoice === "Rock" ? false : true;
+        }
+        showWinner(userWin, userChoice, compChoice);
+    }
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    });
+});
